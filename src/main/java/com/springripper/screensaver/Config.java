3d7f25b85d5a2bb047b793453dcf846b39c625fb
Oleh Soklakov <oleh_soklakov@epam.com>
2017-04-25 13:26:17 +0300

    implement screensaver prototype in singleton

---
+++ src/main/java/com/springripper/screensaver/Config.java
@@ -0,0 +1,29 @@
+package com.springripper.screensaver;
+
+import org.springframework.context.annotation.*;
+
+import java.awt.*;
+import java.util.Random;
+
+@Configuration
+@ComponentScan(basePackages = "com.springripper.screensaver")
+public class Config {
+
+    @Bean
+    @Scope("prototype")
+    //@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public Color color() {
+        Random random = new Random();
+        return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
+    }
+
+    @Bean
+    public ColorFrame colorFrame() {
+        return new ColorFrame() {
+            @Override
+            protected Color getColor() {
+                return color();
+            }
+        };
+    }
+}
